4b5a44368fd5565a294c5ac52818339dc1884a8a
Timur Tuk <tima.xxxyyy@mail.ru>
2026-07-18 17:34:24 +0300

    some goofy LSP things?

--- init.lua
+++ init.lua
@@ -7,13 +7,47 @@ require("config.options")
 require("config.keymaps")
 -- Clear highlights on search when pressing <Esc> in normal mode
 
+-- =============================================================
+-- LSP configuration
+-- =============================================================
+vim.lsp.config('clangd', {
+  cmd = {'clangd'},
+  filetypes = {'c', 'h'},
+})
+
+vim.lsp.enable({'clangd'})
+
+vim.o.autocomplete = true
+
+local diagnostic_signs = {
+  Error = "E",
+  Warn = "W",
+  Hint = "H",
+  Info = "I",
+}
+
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
+      [vim.diagnostic.severity.WARN] = diagnostic_signs.Warn,
+      [vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
+      [vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
+    },
+  },
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {border = "rounded", source = "always", header = "", prefix = "", focusable = false, style = "minimal",},
+})
+-- =============================================================
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
 
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- Installing lazy
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
